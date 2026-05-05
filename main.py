29f7293fa0d29176bfdcb0c99965977f8d192414
语芮澈 <WrcJimmy@outlook.com>
2026-05-05 19:01:48 +0800

    小修小补

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from psutil import boot_time
 
 
-@register("astrbot_plugin_examine", "语芮澈", "功能完善的入群自动考核插件！", "v2.1", "https://github.com/YuRuiChe/astrbot_plugin_examine")
+@register("astrbot_plugin_examine", "语芮澈", "功能完善的入群自动考核插件！", "v2.2", "https://github.com/YuRuiChe/astrbot_plugin_examine")
 class MyPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
@@ -30,11 +30,13 @@ class MyPlugin(Star):
         self.whether_at = group_entry_reminder.get("whether_at", False)
         answer = config.get("answer") or {}
         self.total_number_of_questions = answer.get("total_number_of_questions", 0)
+        self.finally_questions = answer.get("finally_questions", 15)
         self.total_score = answer.get("total_score", 100)
         self.passing_line = answer.get("passing_line", 60)
         self.limited_time = answer.get("limited_time", 100)
+        self.pre_exam_instructions = answer.get("pre_exam_instructions", "考前须知：\n请使用“作答”指令以答题，“确定”指令以结束答题\n示例：\n作答abcabcabcabc（前面一定要有“作答”二字！）\n总共有{self.finally_questions}道题，写多写少会提示\n请于{self.limited_time}秒内完成答题\n\n题目将于{self.read_time}秒后发送")
+        self.read_time = answer.get("read_time", 60)
         self.randomly_selected_questions = answer.get("randomly_selected_questions", False)
-        self.finally_questions = answer.get("finally_questions", 15)
         question_bank = config.get("question_bank") or {}
         self.question = question_bank.get("question", "")
         self.option = question_bank.get("option", "")
@@ -271,8 +273,10 @@ class MyPlugin(Star):
                             except Exception as e:
                                 await event.send(event.plain_result("消息发送失败，请检查后台日志"))
                                 logger.error(f"向群 {group_umo} 发送消息失败: {e}")
-                            yield event.plain_result(f"考核开始，请使用“作答”指令以答题，“确定”指令以结束答题\n示例：\n作答abcabcabcabc")
-                            yield event.plain_result(f"以下为题目，请于{self.limited_time}秒内完成，现在开始计时\n\n{str(out)}")
+                            yield event.plain_result(self.pre_exam_instructions)
+                            logger.info("考前须知！")
+                            time.sleep(self.read_time)
+                            yield event.plain_result(f"考核开始，以下为题目，请于{self.limited_time}秒内完成，现在开始计时\n\n{str(out)}")
                             logger.info("已发送题目！")
                             # ====================注册会话控制器====================
                             # @session_waiter 装饰器：创建一个等待用户回复的会话
@@ -291,11 +295,11 @@ class MyPlugin(Star):
                             # ====================异常处理====================
                             except TimeoutError:
                                 # 用户规定时间内没有回复，触发超时
-                                yield event.plain_result("答题超时！结束考核！请联系管理员处理")
-                                logger.info(f"用户{user_umo}答题超时！结束考核！请联系管理员处理")
+                                yield event.plain_result("答题超时！结束考核！请联系管理员处理，或可尝试再次答题")
+                                logger.info(f"用户{user_umo}答题超时！结束考核！")
                                 try:
                                     result = event.make_result()
-                                    result.chain = [Plain(f"未通过:新人{user_umo}作答超时！")]
+                                    result.chain = [Plain(f"❌未通过:新人{user_umo}作答超时！")]
                                     await self.context.send_message(group_umo, result)
                                     logger.info(f"已向群{group_umo}发送{user_umo}的卡片")
                                 except Exception as e:
@@ -372,11 +376,11 @@ class MyPlugin(Star):
                         except Exception as e:
                             await event.send(event.plain_result("消息发送失败，请检查后台日志"))
                             logger.error(f"向群 {group_umo} 发送消息失败: {e}")
-                        yield event.plain_result(
-                            f"考核开始，请使用“作答”指令以答题，“确定”指令以结束答题\n示例：\n作答abcabcabcabc")
-                        yield event.plain_result(f"以下为题目，请于{self.limited_time}秒内完成，现在开始计时\n\n{str(out)}")
+                        yield event.plain_result(self.pre_exam_instructions)
+                        logger.info("考前须知！")
+                        time.sleep(self.read_time)
+                        yield event.plain_result(f"考核开始，以下为题目，请于{self.limited_time}秒内完成，现在开始计时\n\n{str(out)}")
                         logger.info("已发送题目！")
-
                         # ====================注册会话控制器====================
                         # @session_waiter 装饰器：创建一个等待用户回复的会话
                         # timeout=60: 会话超时时间60秒，超时后会抛出 TimeoutError
@@ -395,11 +399,11 @@ class MyPlugin(Star):
                         # ====================异常处理====================
                         except TimeoutError:
                             # 用户规定时间内没有回复，触发超时
-                            yield event.plain_result("答题超时！结束考核！请联系管理员处理")
-                            logger.info(f"用户{user_umo}答题超时！结束考核！请联系管理员处理")
+                            yield event.plain_result("答题超时！结束考核！请联系管理员处理，或可尝试再次答题")
+                            logger.info(f"用户{user_umo}答题超时！结束考核！")
                             try:
                                 result = event.make_result()
-                                result.chain = [Plain(f"未通过:新人{user_umo}作答超时！")]
+                                result.chain = [Plain(f"❌未通过:新人{user_umo}作答超时！")]
                                 await self.context.send_message(group_umo, result)
                                 logger.info(f"已向群{group_umo}发送{user_umo}的卡片")
                             except Exception as e:
